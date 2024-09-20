6db1326a8421fdb704fc6611a870e02583102cf0
Maggie Zhang <maggiey.zhang@mail.utoronto.ca>
2024-09-20 20:03:12 +0000

    clean data

--- starter_folder-main/scripts/02-data_cleaning.R
+++ starter_folder-main/scripts/02-data_cleaning.R
@@ -2,43 +2,23 @@
 # Purpose: Cleans the raw plane data by Open Data Toronto
 # Author: Maggie Zhang
 # Date: 19 September 2024
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Contact: maggiey.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Have the data downloaded
+# Any other information needed? No
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/raw_marriage_data")
+raw_marriage_data <- read_csv(
+  "starter_folder-main/data/raw_data/raw_marriage_data.csv")
 
 cleaned_data <-
-  raw_data |>
+  raw_marriage_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  separate(col = time_period, into = c("year", "month"), sep = "-") |>
+  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-")))
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "starter_folder-main/data/analysis_data/cleaned_data.csv")
