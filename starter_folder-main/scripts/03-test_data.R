4ba3eea7d7e4d76f981c6ce116422c11a51676f8
Maggie Zhang <maggiey.zhang@mail.utoronto.ca>
2024-09-20 20:35:33 +0000

    further changes

--- starter_folder-main/scripts/03-test_data.R
+++ starter_folder-main/scripts/03-test_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Saniy check of the data
+# Purpose: Test and check of the simulated data
 # Author: Maggie Zhang
 # Date: 19 September 2024
 # Contact: maggiey.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: have the data ready into R
+# Any other information needed? No
 
 
 #### Workspace setup ####
@@ -13,7 +13,7 @@ library(tidyverse)
 
 
 #### Test data ####
-data <- read.csv("simulated_data")
+data <- read.csv("starter_folder-main/data/raw_data/")
 
 # Test for Neg
 data$number
